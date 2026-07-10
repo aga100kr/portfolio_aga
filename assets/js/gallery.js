491b8e032e3cfe6182b4f6a1de6773947f716a38
André Andrade <153104357+aga100kr@users.noreply.github.com>
2026-07-10 10:12:15 +0100

    Create gallery.js

---
+++ assets/js/gallery.js
@@ -0,0 +1,13 @@
+import PhotoSwipeLightbox from './photoswipe-lightbox.esm.js';
+
+const lightbox = new PhotoSwipeLightbox({
+
+    gallery: '.gallery',
+
+    children: 'a',
+
+    pswpModule: () => import('./photoswipe.esm.js')
+
+});
+
+lightbox.init();
